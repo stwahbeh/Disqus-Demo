708a8abb80e108a9942039e80742fa4220dfc011
Seung Lee <shl7@uw.edu>
2015-12-02 17:44:56 -0800

    answer and stuff

--- js/disqus.js
+++ js/disqus.js
@@ -1,79 +1,35 @@
+var ACCESS_TOKEN = "d1a4145e953c4c4e9f0ee0c61c202486";
+var API_KEY = "zFYDrRp7UkXfhX3xWuGaLQfi2T0hBjUeJLAszIKIC0RObnKclNc1yPkDGslOotqB";
+
+// create angular app
+var myApp = angular.module('myApp', []);
 
 
-var data;
-var albumUrl = 'https://api.spotify.com/v1/search?type=album&query=' 
-var trackUrl = 'https://api.spotify.com/v1/albums/'
-var myApp = angular.module('myApp', [])
-var accessToken;
-var search;
+  // get list of threads in asc order
 
+  // inside the div with id
+  // create divs with id of thread's id
+  // put title in h3
+  // put author in
+  // put content in 
 
+  // get list of posts
+  // create divs with content in p
+  // attach them to threads with corresponding id
 
+myApp.controller('myCtrl', function($scope, $http) {
+  $scope.name = "stuff";
+  $scope.posts = {};
 
-//gets featured categories on load
-var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
-
-  $scope.audioObject = {}
-  $scope.getAlbum = function() {
-    $scope.notTracks = false;
-    $scope.notAlbum = true;
-    search = albumUrl + $scope.album 
-    $http.get(search).success(function(response){
-      data = $scope.albums = response.albums.items 
-      $scope.notTracks = !$scope.notTracks; 
-  
-    })
-
-  }
-  //pre-loads categories
-  //$scope.getCategories(); 
-
-$scope.switchView = function(){
-   $scope.notTracks = false;
-   $scope.notAlbum = true;
-   $http.get(search).success(function(response){
-      data = $scope.albums = response.albums.items 
-      $scope.notTracks = !$scope.notTracks; 
-})
- }
-
-// {category_id}/playlists
-  $scope.getTracks = function(category){
-    var id = category;
-    $scope.notTracks = true;
-    $scope.notAlbum = false;
-    $scope.trackObject = {}
-    $http.get(trackUrl + id + '/tracks').success(function(response){  
-    
-    data = $scope.tracks = response.items
-    
-   
-    $scope.notAlbum = !$scope.notAlbum; 
-    
-
-
-    })
-  }
-
-
-  //plays songs
-  $scope.play = function(song) {
-    if($scope.currentSong == song) {
-      $scope.audioObject.pause()
-      $scope.currentSong = false
-      return
-    }
-    else {
-      if($scope.audioObject.pause != undefined) $scope.audioObject.pause()
-      $scope.audioObject = new Audio(song);
-      $scope.audioObject.play()  
-      $scope.currentSong = song
-    }
-  }
-})
-
-
-// Add tool tips to anything with a title property
-$('body').tooltip({
-    selector: '[title]'
+  $http.get('https://disqus.com/api/3.0/forums/listPosts.json', {
+      params: {
+        access_token: ACCESS_TOKEN,
+        api_key: API_KEY,
+        forum: "youtatest1",
+        order: "asc" } 
+  }).success(function(response) {
+    var data = response.response;
+    console.log(data);
+    $scope.posts = data;
+  });
 });
